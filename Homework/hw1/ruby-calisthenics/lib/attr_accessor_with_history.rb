e17a3db5bf183dab7157eb5a3d475b5ce1584580
saasbook <saasbook@saasbook.(none)>
2015-02-17 01:05:16 -0400

    Added code for attr_accessor_with_history.rb

--- Homework/hw1/ruby-calisthenics/lib/attr_accessor_with_history.rb
+++ Homework/hw1/ruby-calisthenics/lib/attr_accessor_with_history.rb
@@ -1,10 +1,31 @@
 class Class
   def attr_accessor_with_history(attr_name)
-    attr_name = attr_name.to_s # make sure it's a string
-    attr_reader attr_name # create the attribute's getter
-    attr_reader attr_name+"_history" # create bar_history getter
-    class_eval %Q{
-      # YOUR CODE HERE
+    attr_name = attr_name.to_s 
+    attr_reader attr_name 
+    attr_reader attr_name + "_history"
+    attr_accessor :past
+    class_eval %{
+      # YO CODE HERE
+      def #{attr_name}=(val)
+        @past = #{attr_name}
+        if @#{attr_name}_history.nil?
+          @#{attr_name}_history = [nil]
+        else
+          @#{attr_name}_history.push(@past)
+        end
+        @#{attr_name} = val
+      end
     }
   end
 end
+
+class Example
+  attr_accessor_with_history :foo
+  attr_accessor_with_history :bar
+end
+a = Example.new; a.foo = 2; a.foo = "test"; 
+puts a.foo_history # => [nil, 2, "test"]
+
+a = Example.new
+
+puts a.foo_history # => [nil]
